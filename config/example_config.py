13cef79565aaf27a9a588b516b928224db68cd20
DE3PM <de3pm.exclusives@gmail.com>
2026-04-10 21:48:44 +0530

    Update example_config.py

--- config/example_config.py
+++ config/example_config.py
@@ -1,23 +1,27 @@
-# config.py v1.0.5
+# config.py v1.0.9
+
+# ============================================
+# MAIN CONFIGURATION
+# ============================================
 
 config = {
     
-    # TMDb API key (required to fetch metadata like title, poster, etc.)
+    # TMDb API key (used to fetch movie/TV metadata like title, poster, cast, etc.)
     # Get your API key here: https://www.themoviedb.org/settings/api
     "TMDb": {
-        "API_KEY": ""
+        "API_KEY": "YOUR_TMDB_API_KEY"
     },
 
-    # Enable/disable IMDb & TMDb info in description
-    "imdb_and_tmdb_info": "True",
+    # Include IMDb & TMDb information in the description (True/False)
+    "imdb_and_tmdb_info": True,
     
     # Directory where upload logs will be stored
-    "uploads_logs_directory": "Downloads/BWT-Uploader/Uploads",
+    "uploads_logs_directory": "Downloads/BWT-Uploader/Uploads", 
     
     # Number of screenshots to capture from the video file
     "screenshots_number": 6,
-    
-    # Number of parallel upload threads
+
+    # Number of parallel threads for uploading screenshots
     "upload_threads": 3,
 
     # Primary image hosting service
@@ -28,36 +32,41 @@ config = {
     "fallback_image_host": "Imgbb",
     "fallback_image_host_2": "Freeimage",
 
-    # API keys for image hosting services (fill only what you use)
+    # API keys for supported image hosts
+    # Only required for the hosts you actually use
     "image_host_api_key": {
-        "Freeimage": "",
-        "Imgbb": "",
-        "Imageride": "",
-        "Lookmyimg": "",
-        "Onlyimg": "",
-        "PTScreen": ""
+        "Freeimage": "YOUR_FREEIMAGE_API_KEY",
+        "Imgbb": "YOUR_IMGBB_API_KEY",
+        "Imageride": "YOUR_IMAGERIDE_API_KEY",
+        "Lookmyimg": "YOUR_LOOKMYIMG_API_KEY",
+        "Onlyimg": "YOUR_ONLYIMG_API_KEY",
+        "PTScreen": "YOUR_PTSCREEN_API_KEY"
     },
 
-    # BWTorrents account configuration
+    # ============================================
+    # BWTorrents Tracker Configuration
+    # ============================================
     "BWTorrents": {
+        # Main site URL (alternatives available if down)
         "base_url": "https://bwtorrents.tv",
-        # Alternative domains:
-        # https://bwtorrents.cc
-        # https://bwtorrents.xyz
-        # https://bwtorrents.us
 
+        # Tracker announce URL
         "announce_url": "https://bwtorrents.tv/announce.php",
-        
-        # Your login credentials
-        "username": "",
-        "password": ""
+
+        # Login credentials
+        "username": "YOUR_USERNAME",
+        "password": "YOUR_PASSWORD"
     },
 
-    # BBCode styling configuration for torrent description
+    # ============================================
+    # BBCode Styling Configuration
+    # ============================================
     "bbcode": {
 
-        # Media info banners
+        # Banner displayed above MediaInfo
         "mediainfo_banner": "[img]https://i.ibb.co/DfF7Pbt/Media-Info.png[/img]",
+
+        # Banner for BD/DVD info (if used)
         "bdinfo_banner": "[img]https://i.ibb.co/DfF7Pbt/Media-Info.png[/img]",
         "dvdinfo_banner": "[img]https://i.ibb.co/DfF7Pbt/Media-Info.png[/img]",
         
@@ -69,33 +78,76 @@ config = {
             "subtitle": "[b][color=teal]★ Subtitle ★[/color][/b]",
             "chapters": "[b][color=red]★ Chapters ★[/color][/b]"
         },
-
-        # Banner displayed above screenshots
-        "screenshot_banner": "[img]https://i.ibb.co/9vZTnQk/Screenshot.png[/img]"
     },
 }
 
 
-# Default BBCode template for torrent description
-# DO NOT remove placeholders:
-# {header}, {file_name}, {media_info}, {screenshot_header}, {screenshot_bbcode}
+# ============================================
+# MAIN DESCRIPTION TEMPLATE
+# ============================================
+# Default BBCode template for the torrent description.
+# NOTE:
+# You may customize the visual style and layout, but DO NOT remove the following placeholders:
+# {detailed_info}, {file_name}, {media_info}, {screenshot_bbcode}, etc.
 
 BBCODE_TEMPLATE = """
-{header}
+{detailed_info}
 
 [center]
 [b][size=4][color=red]
 [font=Arial]{file_name}[/font][/color]
 [/size][/b]
 
-{media_info}
+{info_banner}
 
-{screenshot_header}
+[quote]
+{media_info}
+[/quote]
+
+[img]https://i.ibb.co/9vZTnQk/Screenshot.png[/img]
 
 {screenshot_bbcode}
 
-[b][size=5][color=green][font=Courier New]
-....Enjoying & Keep Seeding....
-[/font][/color][/size][/b]
+[b][size=5][color=green][font=Courier New]....Enjoying & Keep Seeding....[/font][/color][/size][/b]
 [/center]
+"""
+
+
+# ============================================
+# DETAILED MOVIE INFO TEMPLATE (TMDb / IMDb)
+# ============================================
+
+# This block generates rich metadata (poster, cast, ratings, overview)
+# and is injected into the main BBCODE_TEMPLATE using {detailed_info}
+# You may customize the visual style and layout, but DO NOT remove the following placeholders:
+# {title}, ({year}, {poster}, etc.
+
+DETAILED_BBCODE_TEMPLATE = """
+[center]
+[font=Courier New][size=6][color=#00BFFF][b]{title} ({year})[/b][/color][/size][/font]
+
+[img]{poster}[/img]
+
+{imdb_btn}     {tmdb_btn}
+[/center]
+
+[center]━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[/center]
+
+[font=Courier New][size=4]
+[color=#FF9900][b]Genre........:[/b][/color] {genres}  
+[color=#FF9900][b]Released.....:[/b][/color] {release_date}  
+[color=#FF9900][b]Runtime......:[/b][/color] {runtime}  
+[color=#FF9900][b]Category.....:[/b][/color] {category}  
+
+[color=#FF9900][b]Cast.........:[/b][/color] {cast}  
+[color=#FF9900][b]Director.....:[/b][/color] {director}  
+[color=#FF9900][b]Writers......:[/b][/color] {writers}  
+
+[color=#FF9900][b]IMDb Rating..:[/b][/color] [b]{imdb_rating}/10[/b] {imdb_votes}  
+[color=#FF9900][b]TMDb Rating..:[/b][/color] [b]{tmdb_rating}/10[/b] {tmdb_votes}  
+
+[color=#00BFFF][b]Overview.....:[/b][/color] {overview}
+[/size][/font]
+
+[center]━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━[/center]
 """
